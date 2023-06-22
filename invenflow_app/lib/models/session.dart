880ee04560896927c411c7fa8cb39d64b56d5be5
c-mnovelli <c-mnovelli@globalization-partners.com>
2023-06-21 23:18:20 -0300

    log in implementation with the backend

---
+++ invenflow_app/lib/models/session.dart
@@ -0,0 +1,16 @@
+class Session {
+  String email;
+  String password;
+
+  Session({
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+}
--- invenflow_app/lib/repositories/auth/auth_dataprovider.dart
+++ invenflow_app/lib/repositories/auth/auth_dataprovider.dart
@@ -1,10 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:invenflow_app/models/user.dart';
+import 'package:invenflow_app/models/session.dart';
+
+import '../../common/tockenManager.dart';
 
 class AuthDataProvider {
-  static const URL =
-      "http://api-gateway-prod-alb-2091094697.us-east-1.elb.amazonaws.com/api/v1";
+  static const URL = "http://192.168.1.50:8000/api/v1";
 
   Future<int> insertUser(User? user) async {
     final url = Uri.parse('${URL}/auth/company-signup');
@@ -21,6 +23,30 @@ class AuthDataProvider {
       final userId = jsonResponse["user"] as int;
       print('User created sucessfully. This is the userId created: ${userId}');
       return userId;
+    } else {
+      print('Request failed with status code: ${response.statusCode}');
+      throw Exception('Failed to insert user');
+    }
+  }
+
+  Future<String?> logIn(Session? session) async {
+    final url = Uri.parse('${URL}/auth/login');
+    final body = jsonEncode(session?.toJson());
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+
+      final token = jsonResponse["token"] as String;
+      await TokenManager.saveToken(token);
+      String? savedToken = await TokenManager.getToken();
+      print("-> savedToken: ${savedToken}");
+      return savedToken;
     } else {
       print('Request failed with status code: ${response.statusCode}');
       throw Exception('Failed to insert user');
