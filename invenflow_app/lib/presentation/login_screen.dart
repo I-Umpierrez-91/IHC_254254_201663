b9223925bf730ba4ee9c0ed130e446479946e08d
I-Umpierrez-91 <I-Umpierrez-91@users.noreply.github.com>
2023-06-25 23:47:34 -0300

    sacar appbar de login

--- invenflow_app/lib/presentation/login_screen.dart
+++ invenflow_app/lib/presentation/login_screen.dart
@@ -15,7 +15,6 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
